7eae1a66127f42b39de713b515e40ccd74ef19a5
ONS001 <147975242+ONS001@users.noreply.github.com>
2024-01-22 18:49:35 +0000

    Update Auto start expo Bot.js

--- Auto start expo Bot.js
+++ Auto start expo Bot.js
@@ -1,14 +1,21 @@
 TelegramID = enterChatIDHere
+BotName = enterBotNameHere
 
 timeRangeStart = "00:11:00"
 timeRangeEnd = "00:11:05"
 
+StopSleepMode()
+
+if !IsLoggedIn() {
+  Login()
+}
+
 func autoExpo() {
 StartExpeditionsBot()
 
-SendTelegram(TelegramID, "Expediton bot started")
+SendTelegram(TelegramID, "Expediton bot started for bot "{BotName}"")
 }
 
 
-RangeCronExec(timeRangeStart, timeRangeEnd, "Start expedition bot", autoExpo)
+RangeCronExec(timeRangeStart, timeRangeEnd, BotName "Start expedition bot", autoExpo)
 <-OnQuitCh // Blocks forever so that cronexec can be executed
