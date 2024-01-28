d348cede849da864c5d071734e457d4f08af9cf7
ONS001 <147975242+ONS001@users.noreply.github.com>
2024-01-28 08:53:27 +0000

    Update Deploy-Recall Fleetsave.js

--- Deploy-Recall Fleetsave.js
+++ Deploy-Recall Fleetsave.js
@@ -1,16 +1,24 @@
 //////////////////////////////////////
 // Change these settings to your own.
-// If you wish to use this script without it logging out, simply remove the Logout() lines.
-//
+// If you do not want the bot to log you out remove the Logout()
+// The bot will turn off any sleep mode and log back in before the fleet lands to be safe.
 // To set these to planets, simply remove the M: part.
 sendFrom = "M:1:2:3" // Sending fleet from here.
 sendTo = "M:1:2:4" // Sending fleet to here.
+deutToLeave = 1500000 // Enter how much deut to leave behind when fleetsaving.
+
 // Enter your Telegram Chat ID.
 TelegramID = EnterTelegramIDHere
+
 /* Sends at the desired fleet speed, if 20% fleet speed results in a 8 hour flight time one way then - 
 that's how long the total flight time will be. */
 fleetSpeed = TEN_PERCENT // TWENTY_PERCENT ... HUNDRED_PERCENT (Set to whichever speed you desire)
 //////////////////////////////////////
+
+// Calculate deut to take.
+celestial = GetCachedCelestial(sendFrom)
+resources, err = GetResourcesDetails(celestial.GetID())
+deutToTake = resources.Deuterium.Available < deutToLeave
 
 // Variables for Telegram usage.
 universeName = GetUniverseName()
@@ -23,7 +31,9 @@ mainFleet.SetOrigin(sendFrom)
 mainFleet.SetDestination(sendTo)
 mainFleet.SetSpeed(fleetSpeed)
 mainFleet.SetMission(PARK)
-mainFleet.SetAllResources()
+mainFleet.SetAllMetal()
+mainFleet.SetAllCrystal()
+mainFleet.SetDeuterium(deutToTake)
 mainFleet.SetAllShips()
 fleet, err = mainFleet.SendNow()
 
@@ -54,5 +64,6 @@ Logout()
 
 // Logs you back in before your fleet lands, that way if defender is active your fleet is safe when it lands.
 Sleep(Random(half * 800, half * 900))
+StopSleepMode()
 Login()
 SendTelegram(TelegramID, uniPlayerName + " Fleet arriving soon"
