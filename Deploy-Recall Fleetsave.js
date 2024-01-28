d3d045e338311d27a5bb053690c0d1f552b586b6
ONS001 <147975242+ONS001@users.noreply.github.com>
2024-01-28 08:42:56 +0000

    Update Deploy-Recall Fleetsave.js

--- Deploy-Recall Fleetsave.js
+++ Deploy-Recall Fleetsave.js
@@ -3,16 +3,19 @@
 // If you wish to use this script without it logging out, simply remove the Logout() lines.
 //
 // To set these to planets, simply remove the M: part.
-sendFrom = "M:1:2:3" // Enter your moon you're sending from.
-sendTo = "M:1:2:4" // Enter your moon you're sending to.
-
-TelegramID = EnterTelegramIDHere // Your chat ID for Telegram.
-BotName = "EnterBotName" // When messaged via Telegram you will know which bot it is.
-
-/* Sends at the desired fleet speed, if 20% fleet speed results in a 8 hour flight time then,
+sendFrom = "M:1:2:3" // Sending fleet from here.
+sendTo = "M:1:2:4" // Sending fleet to here.
+// Enter your Telegram Chat ID.
+TelegramID = EnterTelegramIDHere
+/* Sends at the desired fleet speed, if 20% fleet speed results in a 8 hour flight time one way then - 
 that's how long the total flight time will be. */
 fleetSpeed = TEN_PERCENT // TWENTY_PERCENT ... HUNDRED_PERCENT (Set to whichever speed you desire)
 //////////////////////////////////////
+
+// Variables for Telegram usage.
+universeName = GetUniverseName()
+playerName = GetCachedPlayer().PlayerName
+uniPlayerName = universeName + " - " + playerName
 
 // Creates a new fleet object for fleetsaving.
 mainFleet = NewFleet()
@@ -28,10 +31,13 @@ fleet, err = mainFleet.SendNow()
 half = fleet.ArriveIn / 2 
 Print("Fleetsaving for: ", ShortDur((half * 2)))
 
+// Telegram Message for Fleetsaving.
+SendTelegram(TelegramID, uniPlayerName + " Fleetsaving for: ", ShortDur((half*2)))
+
 // Logs out the bot.
 Logout()
 
-// Recalls the deploy half-way through the flight with slight randomisations.
+// Recalls the deploy half-way through the flight with slight randomisation
 Sleep(Random(half * 980, half * 1010))
 
 // Logs you in if logged out.
@@ -42,9 +48,11 @@ if !IsLoggedIn() {
 // Waits 3-12 seconds, recalls fleet, waits 3-12 seconds then logs out again.
 (Sleep(Random(3,12)*1000))
 CancelFleet(fleet.ID)
+SendTelegram(TelegramID, uniPlayerName + " Recalled fleet"
 (Sleep(Random(3,12)*1000))
 Logout()
 
 // Logs you back in before your fleet lands, that way if defender is active your fleet is safe when it lands.
 Sleep(Random(half * 800, half * 900))
 Login()
+SendTelegram(TelegramID, uniPlayerName + " Fleet arriving soon"
