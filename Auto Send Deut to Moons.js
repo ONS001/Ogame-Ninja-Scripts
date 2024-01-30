b00475bb60539859d259f5823b8905074097f1c4
ONS001 <147975242+ONS001@users.noreply.github.com>
2024-01-30 13:05:55 +0000

    Update Auto Send Deut to Moons.js

--- Auto Send Deut to Moons.js
+++ Auto Send Deut to Moons.js
@@ -1,15 +1,32 @@
-// This script will automatically top your moons up. 
+// This script will automatically top your moons up with deut.
+// It only uses Largo cargos for now.
+// It doesn't take in to account resources already on the way to the moon.
+// It will not used reserved slots.
 //
 // Set this to how much total deut you want each moon to have. 
 deutPerMoon = 100000000
-//
+
 // Which planet or moon to send the deut from.
 sendDeutFrom = GetCachedCelestial("M:1:2:3")
+
+// Time-delay range in seconds for each mission
+waitPerSend = 4
+
+//---------------------------------
+
+// Get how many slots we can use for the transport missions.
+func getUpdatedSlots() {
+    slots = GetSlots()
+    return slots.Total - slots.InUse - GetFleetSlotsReserved()
+}
+
+// Total slots we can use.
+totalSlots = getUpdatedSlots()
 
 // Get a list of the players moons.
 moon = GetMoons()
 
-// 
+// Do the checks for each moon.
 for moon in GetMoons() {
   coord, err = ConvertIntoCoordinate(moon)
 
@@ -21,7 +38,9 @@ for moon in GetMoons() {
   // Calculate number of cargos required.
   lcNeeded, scNeeded = CalcCargo(deutToTransport)
   
+// Only send if cargos are required.
 if lcNeeded > 0 {
+  
   // Send the mission to each moon.
   mainFleet = NewFleet()
   mainFleet.SetOrigin(sendDeutFrom)
@@ -30,8 +49,10 @@ if lcNeeded > 0 {
   mainFleet.SetMission(TRANSPORT)
   _, fuel = mainFleet.FlightTime()
   mainFleet.SetDeuterium(deutToTransport)
-  mainFleet.AddShips(LargeCargo, lcNeeded)
+  mainFleet.AddShips(LARGECARGO, lcNeeded)
   mainFleet.SendNow()
+  totalSlots--
+  print(totalSlots)
 }
-SleepSec(4)
+SleepSec(waitPerSend)
 }
