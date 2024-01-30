1601546ead400152daf0d3ef3870d4f9601c0915
ONS001 <147975242+ONS001@users.noreply.github.com>
2024-01-30 07:05:05 +0000

    Update Auto Stop Expo.js

--- Auto Stop Expo.js
+++ Auto Stop Expo.js
@@ -1,10 +1,18 @@
+// Automatically stops the expeditions bot in a timeframe you specify.
+
 // Enter your Telegram chat ID and bot or username if you wish.
-TelegramID = enterChatIDHere
-BotName = "enterBotNameHere"
+TelegramID = TELEGRAM_CHAT_ID
 
 // Set the time range for when the script starts.
 timeRangeStart = "09:12:00"
 timeRangeEnd = "12:00:00"
+
+
+//------------------
+// Variables for Telegram usage.
+universeName = GetUniverseName()
+playerName = GetCachedPlayer().PlayerName
+uniPlayerName = universeName + " - " + playerName + ":"
 
 func autoExpo() {
   
@@ -20,9 +28,9 @@ if !IsLoggedIn() {
 StopExpeditionsBot()
 
 // Notifies you on Telegram once the Expediton bot is turned off.
-SendTelegram(TelegramID, BotName + " Expediton bot stopped.")
+SendTelegram(TelegramID, uniPlayerName + " Expediton bot stopped.")
 }
 
 
-RangeCronExec(timeRangeStart, timeRangeEnd, BotName + " Stop expedition bot", autoExpo)
+RangeCronExec(timeRangeStart, timeRangeEnd, uniPlayerName + " Stop expedition bot", autoExpo)
 <-OnQuitCh // Blocks forever so that cronexec can be executed
