8d83a28f10e71c0fae970aadee50a4a145ee62b4
ONS001 <147975242+ONS001@users.noreply.github.com>
2024-01-30 12:34:23 +0000

    Create Auto Send Deut to Moons.js

---
+++ Auto Send Deut to Moons.js
@@ -0,0 +1,35 @@
+// This script will automatically top your moons up. 
+//
+// Set this to how much total deut you want each moon to have. 
+deutPerMoon = 100000000
+//
+// Which planet or moon to send the deut from.
+sendDeutFrom = GetCachedCelestial("M:1:2:3")
+
+// Get a list of the players moons.
+moon = GetMoons()
+
+// 
+for moon in GetMoons() {
+  coord, err = ConvertIntoCoordinate(moon)
+
+  // Calculate deut we need to send to each moon.
+  resources, err = GetResourcesDetails(moon)
+  deutOnMoon = resources.Deuterium.Available
+  deutToTransport = deutPerMoon - deutOnMoon
+
+  // Calculate number of cargos required.
+  lcNeeded, scNeeded = CalcCargo(deutToTransport)
+  
+if lcNeeded > 0 {
+  // Send the mission to each moon.
+  mainFleet = NewFleet()
+  mainFleet.SetOrigin(sendDeutFrom)
+  mainFleet.SetDestination(coord)
+  mainFleet.SetSpeed(HUNDRED_PERCENT)
+  mainFleet.SetMission(TRANSPORT)
+  _, fuel = mainFleet.FlightTime()
+  mainFleet.SetDeuterium(deutToTransport)
+  mainFleet.SetShips(lcNeeded)
+}
+}
