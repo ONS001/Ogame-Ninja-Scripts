86e3993c29c7ef0360b95fc6e559dd85fc3dacdb
ONS001 <147975242+ONS001@users.noreply.github.com>
2024-02-24 15:36:34 +0000

    Update Auto Spam Build RL.js

--- Auto Spam Build RL.js
+++ Auto Spam Build RL.js
@@ -1,6 +1,6 @@
  nbTimes = 20
  nbToBuild = 19
  for i=0; i<nbTimes; i++ {
-     Build("M:1:2:3", ROCKETLAUNCHER, nbToBuild)
-     SleepRandSec(1, 2)
+     Build("M:1:2:3", ROCKETLAUNCHER, nbToBuild)
+     SleepRandSec(1, 2)
  }
